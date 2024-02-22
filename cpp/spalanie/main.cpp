b0d0f01e83f91e3d62ba94f7549657ddb9ed75bb
axerosik <emil5656@op.pl>
2024-02-22 14:01:44 +0000

    studentmachine send

---
+++ cpp/spalanie/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int osoby;
+float odleglosc;
+float spalanie;
+float paliwo;
+float xpaliwo;
+float cenao;
+float cena;
+int stacja;
+
+cout<<"Jaka odległość?"<<endl;
+cin>>odleglosc;
+cout<<"ile osob?"<<endl;
+cin>>osoby;
+cout<<"jakie spalanie"<<endl;
+cin>>spalanie;
+cout<<"cena paliwa za litr"<<endl;
+cin>>paliwo;
+
+xpaliwo=odległosc
+
+}
