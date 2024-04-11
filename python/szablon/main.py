b567ebd1cb5e44925d3f9af37d10d3bc20757466
axerosik <emil5656@op.pl>
2024-04-11 13:05:25 +0000

    wyslanie pracy

--- python/szablon/main.py
+++ python/szablon/main.py
@@ -1,1 +1,12 @@
 print ("witaj swiecie")
+for z in range(0,10):
+	 print (z)
+
+x=float (input("podaj wartosc x"))
+y=float (input("podaj wartosc y"))
+if x>10:
+elif x==10 and y==22:
+	print ("cos sie stalo")
+	else: print("x<10")
+	print ("x jest wiekszy od 10")
+print (x+y)
